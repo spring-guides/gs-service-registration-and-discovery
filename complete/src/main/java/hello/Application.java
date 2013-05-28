2395d7e7055ee3b76889cb6f6f700ab0a7bb548e
Greg Turnquist <gturnquist@gopivotal.com>
2013-05-28 11:14:50 -0400

    Rename Main to Application.
    The standard approach is to make the public static void main exist inside class
    Application.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 
-public class Main {
+public class Application {
 
     public static class Customer {
         private String firstName, lastName;
