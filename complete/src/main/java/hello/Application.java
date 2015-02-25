dcbed04188e1784c46f79ba484c1b14a13fc6931
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-25 09:20:28 -0600

    Clean up relationship database example, using @SpringBootApplication
    Resolves #7

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -4,29 +4,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
-public class Application {
+@SpringBootApplication
+public class Application implements CommandLineRunner {
 
     public static void main(String args[]) {
-        // simple DS for test (not for production!)
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(org.h2.Driver.class);
-        dataSource.setUsername("sa");
-        dataSource.setUrl("jdbc:h2:mem");
-        dataSource.setPassword("");
+        SpringApplication.run(Application.class, args);
+    }
 
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
+    @Override
+    public void run(String... strings) throws Exception {
         System.out.println("Creating tables");
         jdbcTemplate.execute("drop table customers if exists");
         jdbcTemplate.execute("create table customers(" +
                 "id serial, first_name varchar(255), last_name varchar(255))");
 
-        String[] names = "John Woo;Jeff Dean;Josh Bloch;Josh Long".split(";");
-        for (String fullname : names) {
+        String[] fullNames = new String[]{"John Woo", "Jeff Dean", "Josh Bloch", "Josh Long"};
+        for (String fullname : fullNames) {
             String[] name = fullname.split(" ");
             System.out.printf("Inserting customer record for %s %s\n", name[0], name[1]);
             jdbcTemplate.update(
@@ -36,7 +39,7 @@ public class Application {
 
         System.out.println("Querying for customer records where first_name = 'Josh':");
         List<Customer> results = jdbcTemplate.query(
-                "select * from customers where first_name = ?", new Object[] { "Josh" },
+                "select id, first_name, last_name from customers where first_name = ?", new Object[] { "Josh" },
                 new RowMapper<Customer>() {
                     @Override
                     public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
